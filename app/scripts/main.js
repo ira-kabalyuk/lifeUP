6b5d92f42526151a7b06705a515d407a751a489c
ira-kabalyuk <jokky@bigmir.net>
2015-12-27 17:59:55 +0200

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
 console.log('\'Allo \'Allo!');
+
+jQuery(document).ready(function() {
+    $('#carousel-see_samples').carousel({
+  interval: 0
+})
+
+$('#carousel-see_samples.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+});
+
