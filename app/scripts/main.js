d95099ff229230155f75047de6f124a00be02a98
ira-kabalyuk <jokky@bigmir.net>
2015-12-28 21:49:26 +0200

    slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,19 @@
 console.log('\'Allo \'Allo!');
 
-jQuery(document).ready(function() {
-    $('#carousel-see_samples').carousel({
-  interval: 0
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
 })
-
-
-});
-
-
 
