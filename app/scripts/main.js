c682d080854161891935107917d526a113fb2713
ira-kabalyuk <jokky@bigmir.net>
2016-01-05 21:27:08 +0200

    slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,3 +53,22 @@ $('.owl-carousel-1').owlCarousel({
     }
 })
 
+$('.owl-carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        600:{
+            items:1
+        },
+         
+        1000:{
+            items:1
+        }
+    }
+})
+
