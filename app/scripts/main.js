17b5442d6a6d098d8fc14566e459351341dab3c2
ira-kabalyuk <jokky@bigmir.net>
2016-01-31 13:24:28 +0200

    dev

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -88,3 +88,42 @@ $('.search-button').click(function(){
     $('.search-wrap').show();
 })
 
+
+
+
+// Find all YouTube videos
+var $allVideos = $("iframe[src^='<a href="http://www.youtube.com">http://www.youtube.com</a>']"),
+  
+    // The element that is fluid width
+    $fluidEl = $("body");
+  
+// Figure out and save aspect ratio for each video
+$allVideos.each(function() {
+  
+  $(this)
+    .data('aspectRatio', this.height / this.width)
+  
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+  
+});
+  
+// When the window is resized
+$(window).resize(function() {
+  
+  var newWidth = $fluidEl.width();
+  
+  // Resize all videos according to their own aspect ratio
+  $allVideos.each(function() {
+  
+    var $el = $(this);
+    $el
+      .width(newWidth)
+      .height(newWidth * $el.data('aspectRatio'));
+  
+  });
+  
+// Kick off one resize to fix all videos on page load
+}).resize();
+
