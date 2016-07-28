daa6a85d68914f56c0c7ed655718403ed84e5b8c
Volodymyr Stakhovskyi <volodymyr.stakhovsky@TX.local>
2016-07-28 16:27:46 +0300

    upgrade gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,79 +3,106 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
+var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var gulpif = require('gulp-if');
+var minifyCss = require('gulp-minify-css');
 
 var src = {
+    app: 'app/',
     html: 'app/**/*.html',
     js: 'app/scripts/*.js',
-    sass: 'app/styles/*.{scss, css}',
+    sass: 'app/styles/*.scss',
     images: 'app/images/**/*',
-    dist: './dist'
+    tmp: '.tmp/',
+    dist: 'dist/'
 }
 
-gulp.task('html', function () {
-    return gulp.src(src.html)
-        .pipe(gulp.dest(src.dist))
-        .pipe(browserSync.stream());
-});
 
 gulp.task('styles', function () {
     return gulp.src(src.sass)
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'nested'}))
         .pipe(autoprefixer())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(src.dist +'/styles'))
+        .pipe(gulp.dest(src.tmp +'/styles'))
         .pipe(browserSync.stream());
 });
 
+
 gulp.task('scripts', function() {
     return gulp.src(src.js)
-        .pipe(gulp.dest(src.dist + '/scripts'))
         //.pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts'))
+        .pipe(gulp.dest(src.tmp + '/scripts'))
         .pipe(browserSync.stream());
 });
+
 
 gulp.task('images', function() {
     return gulp.src(src.images)
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest(src.dist + '/images'))
+        .pipe(gulp.dest(src.tmp + '/images'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('clean', function() {
-    return del(['dist/']);
-});
 
-gulp.task('copy', ['html', 'styles', 'scripts', 'images'], function() {
-    gulp.src('./bower_components/**/*')
-        .pipe(gulp.dest(src.dist+'/bower_components'));
-
-    gulp.src('./app/fonts/**/*')
-        .pipe(gulp.dest(src.dist+'/fonts'));
-});
-
-gulp.task('serve', ['copy'], function() {
-
+gulp.task('serve', ['styles', 'scripts', 'images'], function() {
     browserSync.init({
-        server: src.dist
+        server: {
+            baseDir: [src.tmp, src.app],
+            routes: {
+                "/bower_components": "bower_components"
+            }
+        }
     });
 
-    gulp.watch(src.html, function(e){
-        gulp.src(e.path)
-            .pipe(gulp.dest(src.dist))
-            .pipe(browserSync.stream());
-    });
     gulp.watch(src.sass, ['styles']);
     gulp.watch(src.js, ['scripts']);
     gulp.watch(src.images, ['images']);
+    gulp.watch(src.html).on('change', browserSync.reload);
 });
 
-gulp.task('build', ['clean', 'copy'], function() {
-    // to do zip
+
+gulp.task('serve:dist', ['build'], function() {
+    browserSync.init({
+        server: src.dist
+    });
 });
+
+
+gulp.task('clean', function() {
+    return del.sync([src.tmp, src.dist]);
+});
+
+
+gulp.task('copy', ['styles', 'images'], function() {
+    gulp.src(src.tmp + '/images/**/*')
+        .pipe(gulp.dest(src.dist + '/images'));
+
+    return gulp.src(src.app + '/fonts/**/*')
+        .pipe(gulp.dest(src.dist + '/fonts'))
+});
+
+
+gulp.task('build', ['clean', 'copy'], function() {
+    var assets = useref.assets({searchPath: ['.tmp', 'app', '.']});
+    return gulp.src(src.html)
+        .pipe(assets)
+        .pipe(gulpif(isFileExt('js'), uglify()))
+        .pipe(gulpif(isFileExt('css'), minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest(src.dist));
+});
+
+
+function isFileExt(ext) {
+    return function(file) {
+        var parts = file.path.split('.');
+        var last = parts[parts.length -1];
+        return (ext === last);
+    }
+}
